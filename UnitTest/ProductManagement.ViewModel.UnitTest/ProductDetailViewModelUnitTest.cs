aeeb7e91712bc32a76db879a9dcb5c51cd31c214
ngvanty03 <ngvanty03@gmail.com>
2026-06-07 15:05:40 +0700

    Support unit test for View Model

---
+++ UnitTest/ProductManagement.ViewModel.UnitTest/ProductDetailViewModelUnitTest.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using ProductManagement.Application;
+using ProductManagement.DTO;
+using ProductManagement.UI.ViewModels;
+using Xunit;
+namespace ProductManagement.Test
+{
+    public class ProductDetailViewModelUnitTest
+    {
+        private readonly Mock<IProductService> _mockProductService;
+        private readonly Mock<IProductCategoryService> _mockProductCateService;
+        private readonly Mock<ILogger<ProductDetailViewModel>> _mockProductDetailViewMOdelLogger;
+        public ProductDetailViewModelUnitTest()
+        {
+            SynchronizationContext.SetSynchronizationContext(
+                new SynchronizationContext());
+
+            _mockProductService = new Mock<IProductService>();
+            _mockProductCateService = new Mock<IProductCategoryService>();
+            _mockProductDetailViewMOdelLogger = new Mock<ILogger<ProductDetailViewModel>>();
+        }
+        [Fact(DisplayName = "Show require message when creating product with the product SKU is empty")]
+        public async Task AsyncSaveCommand_ShouldRequire_SKU()
+        {            
+            var viewModel = new ProductDetailViewModel(1, _mockProductService.Object, _mockProductCateService.Object, _mockProductDetailViewMOdelLogger.Object);
+            viewModel.Product = new ProductDTO()
+            {
+                SKU="",
+                Name="iPhone 17",
+                CategoryId=1,
+                Description="",
+                IsActive=true,
+            };
+            await viewModel.SaveCommand.ExecuteAsync(null);
+            Assert.Equal("Please input the product SKU", viewModel.ErrorMessage);           
+        }
+        [Fact(DisplayName = "Show require message when creating product with the product name is empty")]
+        public async Task AsyncSaveCommand_ShouldRequire_Name()
+        {            
+            var viewModel = new ProductDetailViewModel(1, _mockProductService.Object, _mockProductCateService.Object, _mockProductDetailViewMOdelLogger.Object);
+            viewModel.Product = new ProductDTO()
+            {
+                SKU = "1000",
+                Name = "",
+                CategoryId = 1,
+                Description = "",
+                IsActive = true,
+            };
+            await viewModel.SaveCommand.ExecuteAsync(null);
+            Assert.Equal("Please input the product name", viewModel.ErrorMessage);
+        }
+    }
+}
